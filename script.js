a213e5e9edeb0ed0af3b0de1535484dea6169616
ruijadom <ruijadomingues@gmail.com>
2020-02-15 16:36:43 +0000

    create D3Map class

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 // fetch topo.json data
 // done a callback function which provides the json data as a topology variable
 
-d3.json("custom.topo.json")
-  .then(topology => {
-    console.log(topology);
-    const geojson = topojson.feature(topology, topology.objects["custom.geo"]);
-    const svg = d3
+class D3Map {
+  constructor(topology) {
+    this.svg = d3
       .select("body")
       .append("svg")
       .attr("id", "map");
-    const projection = d3.geoAlbers();
-    const path = d3.geoPath().projection(projection);
     const { height, width } = document
       .getElementById("map")
       .getBoundingClientRect();
-    projection.rotate(-75).fitExtent(
+    const geojson = topojson.feature(topology, topology.objects["custom.geo"]);
+    this.countries = geojson.features;
+    this.projection = d3.geoAlbers();
+    this.projection.rotate(-75).fitExtent(
       [
         [0, 0],
         [width, height]
       ],
       geojson
     );
-
-    svg
+  }
+  drawCountries() {
+    const path = d3.geoPath().projection(this.projection);
+    const countryGroup = this.svg
+      .append("g")
       .selectAll("path")
-      .data(geojson.features)
+      .data(this.countries)
       .enter()
       .append("path")
       .attr("d", path)
-      .attr("fill", "lightgray")
-      .attr("stroke", "white");
+      .attr("stroke", "white")
+      .attr("fill", "lightgray");
+
+    return countryGroup;
+  }
+}
+
+d3.json("data/custom.topo.json")
+  .then(topology => {
+    const map = new D3Map(topology);
+    map.drawCountries();
   })
   // handler to catch any error when fetching data
   .catch(err => console.log("error fetching topojson:", err));
